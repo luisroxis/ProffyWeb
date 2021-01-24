463943228950a034900c0fce0df102680c165b5d
Luis Carlos <luiscroxis@gmail.com>
2021-01-23 21:11:14 -0300

    feat: InputCP TextAreaCP+ TeacherForm

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 
+import warningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css'
 function TeacherForm() {
   return (
     <>
-     <div id="page-teacher-list" className="container">
-        <PageHeader title="Que bom que você quer dar aula"/>
+     <div id="page-teacher-form" className="container">
+        <PageHeader 
+          title="Que bom que você quer dar aula"
+          description="O primeiro passo é preencher este formulario de inscrição."
+        />
+
+        <main>
+          <fieldset>
+            <legend>Seus Dados</legend>
+            <Input name="name" label="Nome Completo" />
+            <Input name="avatar" label="Avatar" />
+            <Input name="whatsapp" label="WhatsApp" />  
+          </fieldset>
+
+          <fieldset>
+            <legend>Sobre a Aula</legend>
+            <Input name="subject" label="Materia" />
+            <Input name="cost" label="Custo da sua hora por aula" />
+          </fieldset>
+
+          <footer>
+            <p>
+              <img src={warningIcon} alt="Aviso Importante"/>
+              Importante! <br/>
+              Preencher todos os dados
+            </p>
+            <button type="button">
+              Salvar Cadastro
+            </button>
+          </footer>
+         </main>
       </div>
     </>
   )
